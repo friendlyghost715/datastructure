b7c2a3ed5c7a367204cc526b94e1fa849e366916
friendlyghost715 <715353751@qq.com>
2020-07-27 12:43:38 -0400

    insertlinklist

--- linklist/linklist.c
+++ linklist/linklist.c
@@ -5,15 +5,36 @@
 LinkList InitLinkList()
 {
 	LinkList linklist = (LinkList)malloc(sizeof(Node));
-	linklist->element = 0;
+	linklist->data = 0;
 	linklist->next = NULL;
 	
 	return linklist;
 }
 
-int main()
+STATUS InsertLinkList(LinkList linklist,ElementType data, int pos)
 {
-	LinkList linklist = InitLinkList();
-	printf("%d",linklist->element);
-	return 0;
-}
+	LinkList p = linklist;
+	int j = 0;
+	while(p!=NULL && j<pos-1)
+	{
+		p = p->next;
+		j++;
+	}
+	if(p==NULL || j>pos-1)
+	{
+		return ERROR;	
+	}
+	Node *q = (Node*)malloc(sizeof(Node));
+	q->data = data;
+	q->next = p->next;
+	p->next = q;
+	return SUCCESS; 
+} 
+	
+	int main() 
+	{
+		LinkList linklist = InitLinkList(); 
+		printf("%d",linklist->data); InsertLinkList(linklist,8,1);
+		printf("%d",linklist->next->data);
+		return 0;
+	}
--- linklist/linklist.h
+++ linklist/linklist.h
@@ -1,8 +1,16 @@
+#define ERROR -1;
+#define SUCCESS 1;
+
+typedef int STATUS;
+
+
 typedef int ElementType;
 
 typedef struct Node{
-	ElementType element;
+	ElementType data;
 	struct Node *next;
 }Node, *LinkList;
 
 LinkList InitLinkList();
+
+ElementType InsertLinkList(LinkList linklist, ElementType data, int pos);
